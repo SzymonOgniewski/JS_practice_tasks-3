adec8ccbc5612276799e552e8078292822c0d47c
Szymon Ogniewski <szymonogniewski00@gmail.com>
2022-11-13 19:06:46 +0100

    .

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+console.log(simpleLightbox);
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach(element => {
+  const link = document.createElement('a');
+  link.classList.add('gallery__link');
+  link.href = element.original;
+  gallery.append(link);
+  const image = document.createElement('img');
+  image.classList.add('gallery__image');
+  image.src = element.preview;
+  image.alt = element.description;
+  link.append(image);
+});
