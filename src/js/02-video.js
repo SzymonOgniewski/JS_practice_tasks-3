5c828fc099c5e1d1ee97e4f5f6fb33505a3819a5
Szymon Ogniewski <szymonogniewski00@gmail.com>
2022-11-13 22:15:20 +0100

    task 1 and 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,28 @@
 import Player from '@vimeo/player';
-console.log(Player)
- const iframe = document.querySelector('iframe');
- const player = new Vimeo.Player(iframe);
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
- player.on('play', function () {
-   console.log('played the video!');
- });
+const savePlayerTime = ({ duration, percent, seconds }) => {
+  localStorage.setItem('videoplayer-current-time', seconds);
+};
 
+player.on('timeupdate', throttle(savePlayerTime, 1000));
+
+const getStoragedTime = () => {
+  const defaultTime = 0;
+  try {
+    const storagedTime = localStorage.getItem('videoplayer-current-time');
+    if (!storagedTime) return defaultTime;
+    const parsedTime = JSON.parse(storagedTime);
+    return Number(parsedTime);
+  } catch (error) {
+    return defaultTime;
+  }
+};
+
+const continueWatching = () => {
+  const lastPlayedTime = getStoragedTime();
+  player.setCurrentTime(lastPlayedTime);
+};
+continueWatching();
