f9c0adc47de6d4598b849fc3128dea9f3829fe96
Szymon Ogniewski <szymonogniewski00@gmail.com>
2022-11-14 19:16:41 +0100

    in prog

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
 
+const data = {};
+const inputEventHandler = event => {
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  data.email = email.value;
+  data.message = message.value;
+};
+
+const saveData = () => {
+  if (data.email !== null || data.message !== null) {
+    localStorage.setItem('feedback-form-state', JSON.stringify(data));
+  }
+};
+
+form.addEventListener('input', inputEventHandler);
+form.addEventListener('input', throttle(saveData, 500));
+
+const availableData = localStorage.getItem('feedback-form-state');
+
+if (availableData) {
+  const dataParsed = JSON.parse(availableData);
+  form.elements.email.value = dataParsed.email;
+  form.elements.message.value = dataParsed.message;
+}
+
+const submitEventHandler = event => {
+  event.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  data.email = email.value;
+  data.message = message.value;
+  console.log(`Email: ${data.email}, Message: ${data.message}`);
+  event.currentTarget.reset();
+  localStorage.clear();
+};
+
+form.addEventListener('sumbit', submitEventHandler);
