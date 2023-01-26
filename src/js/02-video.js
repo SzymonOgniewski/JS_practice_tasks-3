fb97fa47bb0fe73f11c10d9d11c6e0644a3f50f4
Szymon Ogniewski <szymonogniewski00@gmail.com>
2023-01-26 15:00:10 +0100

    upgrades

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,6 +4,7 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 const savePlayerTime = ({ duration, percent, seconds }) => {
+  localStorage.setItem('videoplayer-current-percent', percent);
   localStorage.setItem('videoplayer-current-time', seconds);
 };
 
@@ -12,8 +13,9 @@ player.on('timeupdate', throttle(savePlayerTime, 1000));
 const getStoragedTime = () => {
   const defaultTime = 0;
   try {
+    const vidPercent = localStorage.getItem('videoplayer-current-percent');
     const storagedTime = localStorage.getItem('videoplayer-current-time');
-    if (!storagedTime || storagedTime > 568) return defaultTime;
+    if (!vidPercent || vidPercent > 0.98) return defaultTime;
     const parsedTime = JSON.parse(storagedTime);
     return Number(parsedTime);
   } catch (error) {
